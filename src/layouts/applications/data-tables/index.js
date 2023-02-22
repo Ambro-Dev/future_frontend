b85a69afb93295a68ab8e60db44fb887ead29ea1
SirMrPeter <piotr.ambroziak@4mycreation.com>
2023-02-22 16:12:13 +0100

    added chat

--- src/layouts/applications/data-tables/index.js
+++ src/layouts/applications/data-tables/index.js
@@ -1,41 +1,11 @@
-<<<<<<< HEAD
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable react/button-has-type */
-=======
->>>>>>> a068670239a88aeb9c2cae5ff87709f96a9c7f0e
-/**
-=========================================================
-* Material Dashboard 2 PRO React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
-<<<<<<< HEAD
-
 // Material Dashboard 2 PRO React components
 import MDBox from "components/MDBox";
-=======
-import Card from "@mui/material/Card";
-
-// Material Dashboard 2 PRO React components
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
->>>>>>> a068670239a88aeb9c2cae5ff87709f96a9c7f0e
 
 // Material Dashboard 2 PRO React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-<<<<<<< HEAD
 import { Card, Grid, Toolbar } from "@mui/material";
 
 import { useEffect, useState } from "react";
@@ -141,19 +111,10 @@ function DataTables() {
     }
   };
 
-=======
-import DataTable from "examples/Tables/DataTable";
-
-// Data
-import dataTableData from "layouts/applications/data-tables/data/dataTableData";
-
-function DataTables() {
->>>>>>> a068670239a88aeb9c2cae5ff87709f96a9c7f0e
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox pt={6} pb={3}>
-<<<<<<< HEAD
         <Toolbar sx={(theme) => navbarContainer(theme)}>
           <MDBox pr={1}>
             <Autocomplete
@@ -208,37 +169,11 @@ function DataTables() {
               </div>
               <div className="chat-input-container">
                 <input type="text" placeholder="Type your message here" />
-                <button>Send</button>
+                <button type="button">Send</button>
               </div>
             </Card>
           </Grid>
         </Grid>
-=======
-        <MDBox mb={3}>
-          <Card>
-            <MDBox p={3} lineHeight={1}>
-              <MDTypography variant="h5" fontWeight="medium">
-                Datatable Simple
-              </MDTypography>
-              <MDTypography variant="button" color="text">
-                A lightweight, extendable, dependency-free javascript HTML table plugin.
-              </MDTypography>
-            </MDBox>
-            <DataTable table={dataTableData} />
-          </Card>
-        </MDBox>
-        <Card>
-          <MDBox p={3} lineHeight={1}>
-            <MDTypography variant="h5" fontWeight="medium">
-              Datatable Search
-            </MDTypography>
-            <MDTypography variant="button" color="text">
-              A lightweight, extendable, dependency-free javascript HTML table plugin.
-            </MDTypography>
-          </MDBox>
-          <DataTable table={dataTableData} canSearch />
-        </Card>
->>>>>>> a068670239a88aeb9c2cae5ff87709f96a9c7f0e
       </MDBox>
       <Footer />
     </DashboardLayout>
