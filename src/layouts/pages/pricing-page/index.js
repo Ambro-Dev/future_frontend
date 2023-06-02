db697259081be244fcb8d80418044e8a339aa81d
SirMrPeter <piotrkarolambroziak@gmail.com>
2023-06-02 17:01:21 +0200

    files controller

--- src/layouts/pages/pricing-page/index.js
+++ src/layouts/pages/pricing-page/index.js
@@ -1,5 +1,5 @@
 // Distance Learning React examples
-import { Card, Grid } from "@mui/material";
+import { Card, Grid, Icon } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDButton from "components/MDButton";
 import PageLayout from "examples/LayoutContainers/PageLayout";
@@ -10,6 +10,8 @@ import CastForEducationIcon from "@mui/icons-material/CastForEducation";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import useAxiosPrivate from "hooks/useAxiosPrivate";
+import MDTypography from "components/MDTypography";
+import MDProgress from "components/MDProgress";
 
 function PricingPage() {
   const navigate = useNavigate();
@@ -29,12 +31,19 @@ function PricingPage() {
     },
   });
 
+  const [progress, setProgress] = useState(0);
+
   const [allFiles, setAllFiles] = useState(null);
   const axiosPrivate = useAxiosPrivate();
 
   useEffect(() => {
     axiosPrivate.get("files").then((response) => {
       setAllFiles(response.data);
+      const spaceTaken = response.data.totalDiskSpace;
+      if (spaceTaken > 0) {
+        const progressAmount = (spaceTaken / (1024 * 1024) / 400) * 100;
+        setProgress(progressAmount);
+      }
     });
   }, []);
 
@@ -64,9 +73,38 @@ function PricingPage() {
                 </Grid>
               </Grid>
             </Card>
-            <Card sx={{ padding: 3, marginTop: 2 }}>
-              <MDBox>
-                <MDButton onClick={() => console.log(allFiles)}>Log</MDButton>
+            <Card sx={{ marginTop: 2 }}>
+              <MDBox p={3}>
+                <MDBox
+                  display="grid"
+                  justifyContent="center"
+                  alignItems="center"
+                  bgColor="info"
+                  color="white"
+                  width="3rem"
+                  height="3rem"
+                  shadow="md"
+                  borderRadius="lg"
+                  variant="gradient"
+                >
+                  <Icon fontSize="default">description</Icon>
+                </MDBox>
+                <MDBox mt={2.625}>
+                  <MDTypography variant="h5" fontWeight="medium" textTransform="capitalize">
+                    Space taken by course files
+                  </MDTypography>
+                  <MDBox display="flex" justifyContent="space-between">
+                    <MDTypography variant="body2" color="text" fontWeight="regular">
+                      {allFiles ? allFiles.totalDiskSpace : "0"} kB
+                    </MDTypography>
+                    <MDTypography variant="body2" color="text" fontWeight="regular">
+                      400 GB
+                    </MDTypography>
+                  </MDBox>
+                </MDBox>
+                <MDBox>
+                  <MDProgress value={progress} variant="contained" />
+                </MDBox>
               </MDBox>
             </Card>
           </Grid>
